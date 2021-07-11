269f1138f94ad53bb668295ec2028e010bfa99d6
Bantoe Wrotue <bantoewrotue@Bantoes-MacBook-Air.local>
2021-07-11 15:21:17 +0800

    removing the gitignore file

---
+++ src/utils/apis_keys.js
@@ -0,0 +1,9 @@
+//Geolocalisation api_key
+exports.geocode=()=>{
+    return "35b3e49eb9msh710614215c52d63p1553ddjsn521a549dc8d9";
+}
+
+//weather api_key
+exports.weather=()=>{
+    return 'dbe096282cab7d762545fd54e5d7e71d';
+}
